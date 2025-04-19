ba87c1bdaf8bc2a3522c2e12933ba5422e8ef0c6
justinwongeecs <jwonghelloworld@berkeley.edu>
2025-04-19 12:05:33 -0700

    Initial Commit

---
+++ Campfire/CampfireApp.swift
@@ -0,0 +1,17 @@
+//
+//  CampfireApp.swift
+//  Campfire
+//
+//  Created by Justin Wong on 4/19/25.
+//
+
+import SwiftUI
+
+@main
+struct CampfireApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
